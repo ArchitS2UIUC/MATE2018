7a800a92bf734601bab77ce24b931cb4221d13a1
Archit Srivastava <79420398+Architks1@users.noreply.github.com>
2021-09-12 07:45:42 +0800

    changed comments

--- LEDTest.ino
+++ LEDTest.ino
@@ -13,18 +13,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    byte incomingValue = Serial.read(); // gets each value for the array and add its to the values array pretty OP if say so myself
+    byte incomingValue = Serial.read(); // gets each value for the array and add its to the values array 
     values[currentValue] = incomingValue; //voodoo magic
     currentValue++; //add so that the array index moves
-    if(currentValue > 7){ // since their are only 8 numbers we need, if it exceeds 7, throw out the old values and use the new ones. 
+    if(currentValue > 7){ // since there are only 8 numbers we need, if it exceeds 7, throw out the old values and use the new ones. 
       currentValue = 0; //reset
     }
   }
-  if(values[6] == 1){ // some yum code
+  if(values[6] == 1){ 
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin1, LOW);
   }
-  if(values[6] == 0){ // some more yum code
+  if(values[6] == 0){ 
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, LOW);
   }
